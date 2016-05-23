056dc669a57c4272f10f3dbba8e0bdcf30dd47c1
Øivind Lund <lundzern@gmx.com>
2016-05-23 12:19:05 +0200

    Endret DB kobling

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://lund:lundlund@ec2-52-91-3-55.compute-1.amazonaws.com:27017/dummyDB',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
